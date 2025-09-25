9e9120e23a2c42e37d55f4a2eb915117fe646aac
apereira <abelix897@gmail.com>
2025-09-25 19:34:18 +0100

    propagation wip

--- reactor.c
+++ reactor.c
@@ -6,7 +6,7 @@
 /*   By: apereira <apereira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 16:01:51 by apereira          #+#    #+#             */
-/*   Updated: 2025/09/25 19:26:20 by apereira         ###   ########.fr       */
+/*   Updated: 2025/09/25 19:26:58 by apereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,4 +116,81 @@ static void	add_dependent(struct cell *dependency, struct cell *dependent)
 	node->cell = dependent;
 	node->next = dependency->dependents;
 	dependency->dependents = node;
+}
+
+/* ************************************************************************** */
+/* COMPUTATION & PROPAGATION                                                  */
+/* ************************************************************************** */
+
+static int	compute_cell_value(struct cell *cell)
+{
+	if (cell->type == compute1)
+	{
+		compute1_cell_t	*c1;
+
+		c1 = (compute1_cell_t *)cell;
+		return (c1->compute(c1->dependency->value));
+	}
+	if (cell->type == compute2)
+	{
+		compute2_cell_t	*c2;
+
+		c2 = (compute2_cell_t *)cell;
+		return (c2->compute(
+				c2->dependency1->value, c2->dependency2->value));
+	}
+	return (cell->value);
+}
+
+static bool	update_single_pass(reactor_t *r)
+{
+	struct cell	*cell;
+	int			new_value;
+	bool		any_changed;
+
+	any_changed = false;
+	for (cell = r->cells; cell; cell = cell->next)
+	{
+		if (cell->type == input)
+			continue ;
+		new_value = compute_cell_value(cell);
+		if (new_value == cell->value)
+			continue ;
+		cell->value = new_value;
+		cell->changed_in_propagation = true;
+		any_changed = true;
+	}
+	return (any_changed);
+}
+
+static void	update_values_until_stable(reactor_t *r)
+{
+	while (update_single_pass(r))
+		;
+}
+
+static void	fire_callbacks_for_changed_cells(reactor_t *r)
+{
+	struct cell	*cell;
+
+	for (cell = r->cells; cell; cell = cell->next)
+	{
+		if (cell->changed_in_propagation)
+		{
+			fire_cell_callbacks(cell);
+			cell->changed_in_propagation = false;
+		}
+	}
+}
+
+/*
+** After a change in an input cell, update values and fire callbacks.
+*/
+static void	propagate_changes(struct cell *start_cell)
+{
+	reactor_t	*r;
+
+	r = start_cell->reactor;
+	update_values_until_stable(r);
+	fire_callbacks_for_changed_cells(r);
 }
